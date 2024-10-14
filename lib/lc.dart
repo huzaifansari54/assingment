9166de58adbd100dbd17264c5d84c07fd98ace74
Huzaif ansari <78638431+huzaifansari54@users.noreply.github.com>
2024-10-14 23:27:51 +0530

    init

--- lib/lc.dart
+++ lib/lc.dart
@@ -5,9 +5,12 @@ import 'package:softlab/src/auth/data/data_source/auth_local_data_souece.dart';
 import 'package:softlab/src/auth/data/data_source/auth_remote_data_source.dart';
 import 'package:softlab/src/auth/data/repository/auth_repository_imp.dart';
 import 'package:softlab/src/auth/domain/repository/auth_repository.dart';
+import 'package:softlab/src/auth/domain/use_case/forget_password_use_case.dart';
 import 'package:softlab/src/auth/domain/use_case/login_use_case.dart';
 import 'package:softlab/src/auth/domain/use_case/register_use_case.dart';
+import 'package:softlab/src/auth/domain/use_case/reset_password_use_case.dart';
 import 'package:softlab/src/auth/domain/use_case/user_loged_inUSe_case.dart';
+import 'package:softlab/src/auth/domain/use_case/verfy_otp_use_case.dart';
 
 const timoutDuration = Duration(seconds: 20);
 const conectionTimeOut = Duration(seconds: 5);
@@ -30,5 +33,8 @@ Future<void> setup() async {
         AuthRepository(localDataSource: lc(), remoteDataSource: lc()))
     ..registerSingleton(RegisterUseCase(repository: lc()))
     ..registerSingleton(LoginUseCase(repository: lc()))
-    ..registerSingleton(UserLogedInUseCase(repository: lc()));
+    ..registerSingleton(UserLogedInUseCase(repository: lc()))
+    ..registerSingleton(ForgetPasswordUseCase(repository: lc()))
+    ..registerSingleton(ResetPasswordUseCase(repository: lc()))
+    ..registerSingleton(VerifyOtpUseCase(repository: lc()));
 }
